159354abb2652709d28eb336f8ac30567fb99a82
YRian <kelindeyui@outlook.com>
2018-01-28 20:42:35 +0800

    final version
    最后才发现不能下载，但也能获取到所有的图片url链接
    这章学习的是处理json

--- tuchong/tuchongSipder.py
+++ tuchong/tuchongSipder.py
@@ -1,13 +1,11 @@
 
 import requests
 from bs4 import BeautifulSoup
-import re
 
-url='https://tuchong.com/explore/'
+base_url='https://tuchong.com/explore/'
 header={
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
 }
-# 获取照片类别的url列表
 
 def get_url_sort(url_list,url):
     '''
@@ -33,26 +31,54 @@ def get_url_sort(url_list,url):
     # print(url_list)
     return url_list
 
-def get_pic_list(url):
-    # range为页数
-    for i in range(1,5):
-        part_url=url+'posts?page=%s&count=20&order=weekly' %i
-        data=requests.get(part_url).json()
-        print(data)
-        count=len(data['postList'])
-        print(len(data['postList']))
-        for j in range(count):
-            photo_urls=data['postList'][j]['url']
-            print(photo_urls)
-        # print(len(data['postList']))
-    # print(html)
+def get_page_num(page_sort_url):
+    '''
 
-url_lists=[]
-get_url_sort(url_lists,url)
+    :param page_sort_url: 传入的是照片分类的url
+    :return: 返回该类别的总页数
+    '''
+    html=requests.get(page_sort_url,headers=header).text
+    soup=BeautifulSoup(html,'lxml')
+    tag=soup.find('span',class_='tag-posts').string
+    page_num=int(tag[:-3])//20+2
+    # print(tag)
+    # print(num)
+    return page_num
 
-for pic_url in url_lists:
-    # 转换url格式
-    pic=pic_url.replace('tags','rest/tags')
-    get_pic_list(pic)
-    break
+def get_pic_list(page_num,page_sort_url):
+    '''
+
+    :param page_num:
+    :param url:
+    :return:
+    '''
+    try:
+        for i in range(1,page_num):
+            new_url=page_sort_url+'posts?page=%s&count=20&order=weekly' %i
+            data=requests.get(new_url,headers=header).json()
+            # print(data)
+            count=len(data['postList'])
+            # print(len(data['postList']))
+            for j in range(count):
+                photo_urls=data['postList'][j]['url']
+                print(photo_urls)
+            print('--------第{}页--------'.format(i))
+            # print(len(data['postList']))
+        # print(html)
+        return photo_urls
+    except:
+        print('--------------空-------------------')
+
+def main():
+    url_lists=[]
+    get_url_sort(url_lists,base_url)
+
+    for url in url_lists:
+        # 转换url格式
+        page_num= get_page_num(url)
+        pic_url=url.replace('tags','rest/tags')
+        get_pic_list(page_num,pic_url)
+        # 关闭break可以获取全类别的url
+        break
 # print(url_lists)
+main()
